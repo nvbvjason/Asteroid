fba8782895c0370b561dc8954a125dc2d6a5698b
nvbvjason <jasongadewoltz@gmail.com>
2024-12-15 15:04:43 +0100

    left right rotation

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def main():
 
     while (True):
         screen.fill(color="black")
+        
         player.draw(screen)
+        player.update(dt)
 
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000
-
         pygame.display.flip()
 
 if __name__ == "__main__":
