5e2fd087f66decfe466641de027f1594fd4bc998
nvbvjason <jasongadewoltz@gmail.com>
2024-12-15 14:59:27 +0100

    player and Circleshape classes

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
+from player import Player
 from constants import * 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while (True):
         screen.fill(color="black")
+        player.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
