5764883bd69c7167cb063daf53b81c99fff514e8
nvbvjason <jasongadewoltz@gmail.com>
2024-12-15 14:41:51 +0100

    add fps controll

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while (True):
         screen.fill(color="black")
@@ -18,7 +20,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        dt = clock.tick(60) / 1000
 
         pygame.display.flip()
 
